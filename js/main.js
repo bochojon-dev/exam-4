8cf3079d44c8b36a2aadb716aa3df3039ece5d33
Bochojon-dev <maniyevmaqadjon9@gmail.com>
2024-02-20 09:17:45 +0500

    tugadi

--- js/main.js
+++ js/main.js
@@ -15,19 +15,19 @@ function toggleBacktop() {
 }
 // Responsive nav //////////////////////////////////
 function toggleNavbar() {
-  document.getElementById("navbar-responsive").classList.toggle("open");
+  document.getElementById("nav-responsive").classList.toggle("open");
 }
-document.getElementById("menu-btn").addEventListener("click", toggleNavbar);
+document.getElementById("menu-button").addEventListener("click", toggleNavbar);
 // Nav shrink ////////////////////////
 window.addEventListener("scroll", function () {
   shrink();
 });
-let navbar = document.getElementById("navbar");
+let navbar = document.getElementById("nav");
 function shrink() {
   if (scrollY > 100) {
-    navbar.classList.add("navbar-shrink");
+    navbar.classList.add("nav-shrink");
   } else {
-    navbar.classList.remove("navbar-shrink");
+    navbar.classList.remove("nav-shrink");
   }
 }
 // Lght dark ////////////////////////
